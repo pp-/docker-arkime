d43c12f4e98f9c011a3ee7b1b5116d87ed132a4e
mammo0 <marc.ammon@hotmail.de>
2019-12-04 08:32:44 +0100

    fixed echo newline character not considered

--- scripts/startmoloch.sh
+++ scripts/startmoloch.sh
@@ -17,7 +17,7 @@ else
     # possible update
     read old_ver < $MOLOCHDIR/etc/.initialized
     # detect the newer version ($MOLOCH_VERSION contains the actual used version)
-    newer_ver=`echo "$old_ver\n$MOLOCH_VERSION" | sort -rV | head -n 1`
+    newer_ver=`echo -e "$old_ver\n$MOLOCH_VERSION" | sort -rV | head -n 1`
     if [ "$MOLOCH_VERSION" = "$newer_ver" ]; then
         echo "Upgrading ES database..."
         $MOLOCHDIR/bin/Configure
